c404721ab089cd15c95fd4f144c369c989df668f
Alexandr Golomshtok <sgolomshtoks@gmail.com>
2025-01-11 11:20:23 +0300

    Edit main.py
    Улучшена читаемость кода
    Добавлена проверка на существование пользователя в бд для исключения дублирования данных
    Добавленны комментарии
    Добавлена проверка файла .env

--- main.py
+++ main.py
@@ -6,22 +6,23 @@ import datetime
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-
-
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
+# Загрузка переменных окружения из файла .env, если он существует
 if exists(".env"):
     load_dotenv()
 
 
+# Инициализация SQLAlchemy для работы с базой данных
 users_engine = create_engine("sqlite:///users.db")
 Base = declarative_base()
 Session = sessionmaker(bind=users_engine)
 session = Session()
 
 
+# Определение модели пользователя
 class User(Base):
 
     __tablename__ = "users"
@@ -34,10 +35,14 @@ class User(Base):
     joined_at = Column(DateTime, default=datetime.datetime.now)
 
 
+# Создание таблиц, если они еще не существуют
 Base.metadata.create_all(users_engine)
 
 
+# Чтение токен бота из переменной окружения
 BOT_TOKEN: str = getenv('BOT_TOKEN')
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не найден. Проверьте файл .env или переменные окружения.")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
@@ -48,12 +53,15 @@ async def start(message: types.Message):
     await bot.send_message(message.chat.id, "Добро пожаловать в бункер!\nЧтобы начать играть нужно создать лобби \
 - /create\nИли подключится к уже созданному - /join <ID>\nЕсли возникнут вопросы - /help")
 
-    new_user = User(user_id=message.from_user.id,
-                    username=message.from_user.username,
-                    first_name=message.from_user.first_name,
-                    last_name=message.from_user.last_name,
-                    language_code=message.from_user.language_code)
-    session.add(new_user)
+    # Проверка на наличие пользователя в базе
+    existing_user = session.query(User).filter_by(user_id=message.from_user.id).first()
+    if not existing_user:
+        new_user = User(user_id=message.from_user.id,
+                        username=message.from_user.username,
+                        first_name=message.from_user.first_name,
+                        last_name=message.from_user.last_name,
+                        language_code=message.from_user.language_code)
+        session.add(new_user)
     session.commit()
 
 
