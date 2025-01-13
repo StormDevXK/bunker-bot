f430fc118c4da79f69556b057a564cf199597b75
Alexandr Golomshtok <sgolomshtoks@gmail.com>
2025-01-13 11:49:02 +0300

    Edit database.py
    Таблица переименнована в соответствии с проектом бд

--- handlers.py
+++ handlers.py
@@ -1,5 +1,5 @@
 from aiogram import types, Bot
-from database import save_user_data_to_db
+from database import save_user_data_to_db, create_lobby, join_lobby, exit_lobby
 
 
 # Обработчик команды /start
@@ -23,3 +23,47 @@ async def help_handler(message: types.Message, bot: Bot):
         "/join <ID> - Подключиться к существующей игре.\n"
         "/exit - Выйти из игры."
     )
+
+
+# Оработчик команды /create
+async def create_handler(message: types.Message, bot: Bot):
+
+    # Создание lobby и получение его id
+    lobby_id = await create_lobby(message.from_user.id)
+
+    await bot.send_message(
+        message.chat.id, f"Вы создали лобби с айди {lobby_id}\n"
+        "Поделитесь им чтобы другие игроки смогли подключиться к вам."
+    )
+
+
+# Оработчик команды /join
+async def join_handler(message: types.Message, bot: Bot):
+
+    try:
+        lobby_id = int(message.text.split()[1])
+    except (IndexError, ValueError):
+        await message.reply("Пожалуйста, укажите ID игры в формате /join <ID>.")
+        return
+
+    # Подключение к lobby по id
+    try:
+        await join_lobby(message.from_user.id, lobby_id)
+    except ():
+        await message.reply("Пожалуйста, перепроверте ваш <ID>")
+        return
+
+    await bot.send_message(message.chat.id, f"Вы присоединились к лобби с айди {lobby_id}")
+
+
+# Оработчик команды /exit
+async def exit_handler(message: types.Message, bot: Bot):
+
+    # Выход из лобби
+    try:
+        await exit_lobby(message.from_user.id)
+    except ():
+        await message.reply("Вы не находитесь в лобби")
+        return
+
+    await bot.send_message(message.chat.id, f"Вы вышли из лобби.")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from utils import BOT_TOKEN
-from handlers import start_handler, help_handler
+from handlers import start_handler, help_handler, create_handler, join_handler, exit_handler
 from database import init_db
 
 bot = Bot(token=BOT_TOKEN)
@@ -11,6 +11,9 @@ dp = Dispatcher()
 # Регистрация обработчиков
 dp.message.register(start_handler, Command("start"))
 dp.message.register(help_handler, Command("help"))
+dp.message.register(create_handler, Command("create"))
+dp.message.register(join_handler, Command("join"))
+dp.message.register(exit_handler, Command("exit"))
 
 
 async def main():
